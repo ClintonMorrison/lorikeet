c8a15cba74ba4a180706e7e0a8d622e2a72be372
Clinton Morrison <clintonmorrison2@gmail.com>
2022-05-21 14:47:03 -0400

    do not run container after building

--- build.sh
+++ build.sh
@@ -9,6 +9,6 @@ cd ./ui &&
   cd - 
 
 docker build -t "clintonmorrison/projects:lorikeet-$VERSION" . &&
-  docker push "clintonmorrison/projects:lorikeet-$VERSION" &&
-  echo "Running container..." &&
-  sh run_container.sh
+  docker push "clintonmorrison/projects:lorikeet-$VERSION" 
+#  echo "Running container..." &&
+#  sh run_container.sh
