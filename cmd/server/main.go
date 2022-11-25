12db0c7e91afd83e2ad468300999c292073d7aa2
Clinton Morrison <clintonmorrison2@gmail.com>
2022-11-24 19:15:53 -0500

    use environment variable for secret

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/ClintonMorrison/lorikeet/internal/config"
 	"github.com/ClintonMorrison/lorikeet/internal/server"
 )
 
 func main() {
+	recaptchaSecret := os.Getenv("LORIKEET_RECAPTCHA_SECRET")
+
+	if recaptchaSecret == "" {
+		fmt.Println("Environment variable 'LORIKEET_RECAPTCHA_SECRET' is not set")
+		os.Exit(1)
+	}
+
 	server.Run(
 		config.DATA_PATH,
 		config.SERVER_ADDRESS,
-		"", // TODO: use environment variable DO NOT COMMIT
+		os.Getenv("LORIKEET_RECAPTCHA_SECRET"),
 		config.LOG_PATH,
 		config.REQUEST_LOG_PATH,
 		config.ERROR_LOG_PATH)
