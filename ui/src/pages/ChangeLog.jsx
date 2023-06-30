2165069760db6549767f0f94b3256920ade54ff7
Clinton Morrison <clintonmorrison2@gmail.com>
2023-06-30 17:32:47 -0400

    add new logo image

--- ui/src/pages/ChangeLog.jsx
+++ ui/src/pages/ChangeLog.jsx
@@ -11,6 +11,15 @@ export default class ChangeLog extends React.Component {
 
         <h1>Change Log</h1>
 
+        <h2>Saturday, July 12, 2023</h2>
+        <ul className="browser-default">
+          <li>security improvements</li>
+          <li>bug fixes</li>
+          <li>updated assets</li>
+          <li>posted notice about account migration</li>
+        </ul>
+
+
         <h2>Saturday, Dec 3, 2022</h2>
         <ul className="browser-default">
           <li>add Recaptcha</li>
