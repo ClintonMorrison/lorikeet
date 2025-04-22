9ad0a7781bf9342f906f2fcdcee2fdc77c37bcc4
Clinton Morrison <clintonmorrison2@gmail.com>
2025-04-21 22:24:42 -0400

    update build script

--- build.sh
+++ build.sh
@@ -1,14 +1,17 @@
 
-. ./version.sh
+#!/bin/bash
+source version.sh
 
-docker stop lorikeet
-docker rm lorikeet
+# docker stop lorikeet
+# docker rm lorikeet
+
+TAG="$DOCKER_REGISTRY_PROJECTS/lorikeet:$VERSION"
 
 cd ./ui &&
+  npm i &&
   npm run build &&
-  cd - 
-
-docker build -t "clintonmorrison/projects:lorikeet-$VERSION" . &&
-  docker push "clintonmorrison/projects:lorikeet-$VERSION" 
+  cd - &&
+  docker build --platform linux/amd64 -t "$TAG" . &&
+  docker push "$TAG" 
 #  echo "Running container..." &&
 #  sh run_container.sh
